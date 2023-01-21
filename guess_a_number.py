453a420e48fd530855238ca80088a75730b3d986
mustanska <122823838+mustanska@users.noreply.github.com>
2023-01-21 23:22:07 +0200

    Added levels

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,32 +1,48 @@
 from random import randint
 from colorama import Fore
 
-random_number = randint(1, 100)
-attempts = 5
+upper_bored = 100
 
-while True:
-    if attempts <= 0:
-        print(Fore.RED + "Sorry! You had 5 attempts and you don't guess a number!")
-        break
+# Loop for all levels in the game
+for level in range(1, 6):
+    random_number = randint(1, upper_bored)
+    attempts = 5
 
-    print(Fore.BLACK + f"You have {attempts} attempts to the end of a game.")
-    player = input("Guess a number that is between 1 and 100 inclusive:")
+    # Loop for the current level
+    while True:
+        # If the attempts is more than 5, the game is over.
+        if attempts <= 0:
+            print(Fore.RED + "Sorry! You had 5 attempts and you don't guess a number!")
+            print(f"You lose the game on {level} level.")
+            exit()
 
-    if not player.isdigit():
-        print(Fore.RED + "Invalid input. Try again.")
-        attempts -= 1
-        continue
+        # Shows how many attempts you have and you should input your choice
+        print(Fore.BLACK + f"You have {attempts} attempts to the end of a game.")
+        player = input(f"Guess a number that is between 1 and {upper_bored} inclusive:")
 
-    player_number = int(player)
+        if not player.isdigit():
+            print(Fore.RED + "Invalid input. Try again.")
+            attempts -= 1
+            continue
 
-    if player_number == random_number:
-        print(Fore.GREEN + "You found a number. Congratulations!")
-        break
-    elif player_number < random_number:
-        attempts -= 1
-        print("Less than a number you should found.")
-        continue
-    elif player_number > random_number:
-        attempts -= 1
-        print("Large than a number you should found.")
-        continue
+        player_number = int(player)
+
+        # Checks if the number is correct, or if it is less than or greater than
+        if player_number == random_number:
+            print(Fore.GREEN + "You found a number. Congratulations!")
+            print("\n")
+            if level == 5:
+                print("You win the GAME!")
+            else:
+                upper_bored += 50
+            break
+
+        elif player_number < random_number:
+            attempts -= 1
+            print("Try with GREATER than that.")
+            continue
+
+        elif player_number > random_number:
+            attempts -= 1
+            print("Try with LESS than that.")
+            continue
