b3c205e58b2169579ae219d12018f545f9a3b85e
mustanska <122823838+mustanska@users.noreply.github.com>
2023-01-21 22:52:13 +0200

    Added border to the attempts

--- guess_a_number.py
+++ guess_a_number.py
@@ -2,12 +2,19 @@ from random import randint
 from colorama import Fore
 
 random_number = randint(1, 100)
+attempts = 5
 
 while True:
-    player = input(Fore.BLACK + "Guess a number that is between 1 and 100 inclusive:")
+    if attempts <= 0:
+        print(Fore.RED + "Sorry! You had 5 attempts and you don't guess a number!")
+        break
+
+    print(Fore.BLACK + f"You have {attempts} attempts to the end of a game.")
+    player = input("Guess a number that is between 1 and 100 inclusive:")
 
     if not player.isdigit():
         print(Fore.RED + "Invalid input. Try again.")
+        attempts -= 1
         continue
 
     player_number = int(player)
@@ -16,8 +23,10 @@ while True:
         print(Fore.GREEN + "You found a number. Congratulations!")
         break
     elif player_number < random_number:
-        print(Fore.RED + "Less than a number you should found.")
+        attempts -= 1
+        print("Less than a number you should found.")
         continue
     elif player_number > random_number:
-        print(Fore.RED + "Large than a number you should found.")
+        attempts -= 1
+        print("Large than a number you should found.")
         continue
